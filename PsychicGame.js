7d3da3f65a0c2d4666d7923765fa205a3e636fcf
Jeff Miller <jeffmiller@Jeffs-MacBook-Pro.local>
2019-09-06 15:09:17 -0500

    homework

--- PsychicGame.js
+++ PsychicGame.js
@@ -1,19 +1,31 @@
+var wins = 0
+var losses = 0
+var guessLeft = 3
+var guessSoFar = []
 
-    
-    var alphabet = ["a","b","c","d","e","f"];
-    var compIndex = Math.floor(Math. random() * alphabet.length);
-    console.log("CompIndex" + compIndex);
-    var compChoice = alphabet + [compIndex];
-    console.log("Secret Answer: " + compChoice);
 
-    document.onkeyup = function(event) {
-        var letter = event.key.toLowerCase();
-        console.log(letter);
-        if(letter === compChoice){
-                alert("You Win!");
-             } else {
-                 alert("You Lost!");
-             }
 
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var compIndex = Math.floor(Math.random() * alphabet.length);
+console.log("CompIndex" + compIndex);
+var compChoice = alphabet + [compIndex];
+console.log("Secret Answer: " + compChoice);
+
+document.onkeyup = function (event) {
+    var letter = event.key.toLowerCase();
+    console.log(letter);
+    if (letter === compChoice) {
+        wins++;
+        guessLeft = 3;
+        alert("You Win!");
+        guessSoFar = []
+
+    } else {
+        alert("You Lost!");
+
+     losses++
+     guessLeft = 3
+     guessSoFar = []
+    }
 
 }
